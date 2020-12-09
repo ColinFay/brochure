a77795bc34d3eac93de8bc4a18533c0c0171406e
colin <colin@thinkr.fr>
2020-12-09 22:38:41 +0100

    Get and set cookies, will close #7

--- R/brochureApp.R
+++ R/brochureApp.R
@@ -15,14 +15,19 @@ brochureApp <- function(
   onStart = NULL,
   options = list(),
   enableBookmarking = NULL,
-  content_404 = "Not found"
+  content_404 = "Not found",
+  with_cookie = TRUE,
+  # Use a function as cookie storage, this allows to
+  # pass your own
+  cookie_storage = local_cookie
 ){
   # We add this enabled, just to be sure
   # `brochure_enable` is called inside a
   # `brochureApp`
   ...multipage_opts$enabled <- TRUE
+
   # Force UI if it hasn't been evaluated yet
-  # So that we are sure `...multipage` are
+  # So that we are sure `...multipage`  and `...multipage_opts`
   # enable
   if (is.function(ui)) ui()
   res <- shinyApp(
@@ -36,12 +41,12 @@ brochureApp <- function(
   old_httpHandler <- res$httpHandler
   res$httpHandler <- function(req){
 
+    httpResponse <- getFromNamespace("httpResponse", "shiny")
     # Handle redirect
     if (req$PATH_INFO %in% ...multipage_opts$redirect$from){
       dest <- ...multipage_opts$redirect[
         ...multipage_opts$redirect$from == req$PATH_INFO,
       ]
-      httpResponse <- getFromNamespace("httpResponse", "shiny")
       return(httpResponse(
         status = dest$code,
         headers = list(
@@ -50,9 +55,29 @@ brochureApp <- function(
       ))
     }
 
+    # Handle logout form,
+    # we want to remove the cookie
+    if (req$PATH_INFO %in% ...multipage_opts$logout$from){
+      dest <- ...multipage_opts$logout[
+        ...multipage_opts$logout$from == req$PATH_INFO,
+      ]
+      # Remove the cookie from the storage,
+      # This will allow to issue a new one when the page reloads
+      # as it won't be valid anymore
+      cookie_storage()$delete_cookie(
+        parse_cookie(req$HTTP_COOKIE)["brochure_session"]
+      )
+      return(httpResponse(
+        status = 302,
+        headers = list(
+          Location = dest$to
+        )
+      ))
+    }
+
+
     # Returning a 404 if the page doesn't exist
     if (!req$PATH_INFO %in% names(...multipage)){
-      httpResponse <- getFromNamespace("httpResponse", "shiny")
       return(httpResponse(
         status = 404,
         content = as.character(content_404)
@@ -61,10 +86,29 @@ brochureApp <- function(
     # Setting the path info for reuse in brochure()
     ...multipage_opts$path <- req$PATH_INFO
 
-
     # Note to self:
     # req$HTTP_COOKIE
-    old_httpHandler(req)
+    # session$request$HTTP_COOKIE
+    inter <- old_httpHandler(req)
+    if (with_cookie){
+      # browser()
+      current_cookie <- parse_cookie(req$HTTP_COOKIE)["brochure_session"]
+      if (
+        is.na(current_cookie) |
+        ! cookie_storage()$is_valid(
+          current_cookie
+        )
+      ){
+        inter$headers <- list(
+          "Set-Cookie" = sprintf(
+            "brochure_session=%s; HttpOnly; Expires=Wed, 21 Oct 2050 07:28:00 GMT;Path=/",
+            cookie_storage()$add_cookie()
+          )
+        )
+      }
+    }
+
+    inter
   }
   res
 }
---
+++ R/cookie.R
@@ -0,0 +1,74 @@
+#' Get the Brochure Cookie
+#'
+#' @param session A shiny sessino object
+#'
+#' @return A string with the cookie, or NA if there is no cookie.
+#' @export
+get_brochure_cookie <- function(session = shiny::getDefaultReactiveDomain()){
+
+  parse_cookie(session$request$HTTP_COOKIE)["brochure_session"]
+
+}
+
+parse_cookie <- function(txt){
+  couples <- strsplit(txt, ";")[[1]]
+  res <- lapply(
+    couples,
+    function(x) {
+      #browser()
+      nms <- strsplit(x, "=")[[1]]
+      res <- gsub("^ +", "", nms[2])
+      res <- gsub("[ ^]* +$", "", res)
+      names(res) <- gsub("^ +", "", nms[1])
+      names(res) <- gsub("[ ^]* +$", "", names(res))
+      res
+    }
+  )
+  unlist(res)
+}
+
+
+local_cookie <- function(){
+  list(
+    get_all = function(name){
+      cookie_file <- system.file("cookies.txt", package = "brochure")
+      if (file.exists(cookie_file)){
+        readLines(
+          cookie_file
+        )
+      } else {
+        return(c(""))
+      }
+    },
+    add_cookie = function(){
+      all <- local_cookie()$get_all()
+      is_valid <- FALSE
+      while (!is_valid) {
+        new_cookie <- digest::digest(
+          c(sample(1e5), Sys.time())
+        )
+        if (!new_cookie %in% all){
+          is_valid <- TRUE
+          write(
+            new_cookie,
+            # bypass the non existance of the file when first cookie is written
+            file.path(system.file( package = "brochure"), "cookies.txt"),
+            append = TRUE
+          )
+        }
+      }
+      new_cookie
+    },
+    delete_cookie = function(which){
+      all <- local_cookie()$get_all()
+      all <- all[all != which]
+      write(
+        all,
+        system.file("cookies.txt", package = "brochure")
+      )
+    },
+    is_valid = function(which){
+      which %in% local_cookie()$get_all()
+    }
+  )
+}
