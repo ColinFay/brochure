1f1eb29f0bfb5933d9676c2025f2103f6489e096
Colin Fay <contact@colinfay.me>
2021-02-11 22:31:30 +0100

    handlers can be passed as formulas (#21)

--- R/req_res_handlers.R
+++ R/req_res_handlers.R
@@ -1,7 +1,9 @@
+#' @importFrom rlang as_function
 add_req_handlers_page <- function(
   href,
   req_handlers
 ){
+  req_handlers <- lapply(req_handlers, as_function)
   ...multipage_opts$req_handlers_page[[href]] <- req_handlers
 }
 
@@ -21,10 +23,12 @@ get_req_handlers_app <- function(){
   ...multipage_opts$req_handlers
 }
 
+#' @importFrom rlang as_function
 add_res_handlers_page <- function(
   href,
   res_handlers
 ){
+  res_handlers <- lapply(res_handlers, as_function)
   ...multipage_opts$res_handlers_page[[href]] <- res_handlers
 }
 
